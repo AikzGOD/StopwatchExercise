a9cc98e1fb9ad1d4cf017fe0caecab0ba51d26cc
Aikz <rauldiogoms@gmail.com>
2023-07-19 10:17:57 +0100

    Adicionar arquivos de projeto.

---
+++ Stopwatch Exercise/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch();
+        }
+
+        public static void StopWatch()
+        {
+            //Instanciar objeto
+            Stopwatch sw = new Stopwatch();
+
+
+            Console.WriteLine("Press enter to start the timer");
+            string userInput = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(userInput))
+            {
+                //Start the timer
+                sw.StartTime = sw.Start();
+
+                Console.WriteLine("Timer Started! Click enter to stop");
+                string stopTimerInput = Console.ReadLine();
+
+
+                if (String.IsNullOrEmpty(stopTimerInput))
+                {
+                    sw.EndTime = sw.Stop();
+
+                    Console.WriteLine("Inicio : " + sw.StartTime);
+                    Console.WriteLine("Fim : " + sw.EndTime);
+                    Console.WriteLine("");
+
+
+                    Console.WriteLine(sw.CalculateDuration());
+                    Console.WriteLine("TimeSpan duration :" + sw.TimeSpent);
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Press enter to restart the timer");
+                    string restartTimerInput = Console.ReadLine();
+
+                    if(String.IsNullOrEmpty(restartTimerInput))
+                    {
+                        Console.Clear();
+                        StopWatch();
+                    }
+
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Stopwatch Exercise/Stopwatch.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch_Exercise
+{
+    internal class Stopwatch
+    {
+        private TimeSpan startTime;
+
+        private TimeSpan endTime;
+
+        private TimeSpan timeSpent;
+
+            public TimeSpan StartTime
+            {
+                get { return startTime; }
+                set { startTime = value; }
+            }
+
+            public TimeSpan EndTime
+            {
+                get { return endTime; }
+                set { endTime = value; }
+            }
+
+            public TimeSpan TimeSpent
+            {
+                get { return timeSpent; }
+                set { timeSpent = value; }
+            }
+
+
+        public TimeSpan Start()
+        {
+            TimeSpan timeStarted = GiveTime();
+
+            return timeStarted;
+        }
+
+        public TimeSpan Stop()
+        {
+            TimeSpan timeEnded = GiveTime();
+
+            return timeEnded;
+        }
+
+        private TimeSpan GiveTime() {
+
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+            return currentTime;
+
+        }
+
+        public string CalculateDuration()
+        {
+            timeSpent = EndTime - StartTime;
+
+            string strTimeSpent = "Duration (Showed in a String) : " + timeSpent.Hours + ":" + timeSpent.Minutes + ":" + timeSpent.Seconds;
+
+
+
+            return strTimeSpent;
+
+
+        }
+
+        
+            
+
+
+    }
+}
